f4afe0047275615ccf4fc9e7b0869f5a24dfdb53
yoshow <ruanyu@live.com>
2017-09-02 01:07:02 +0800

    修改页面

--- src/main/java/com/x3platform/web/controllers/HomeController.java
+++ src/main/java/com/x3platform/web/controllers/HomeController.java
@@ -6,14 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 	/**
 	 * 登陆界面
 	 */
-	@RequestMapping("hello")
+	// @RequestMapping("upload")
+	@RequestMapping(value = "/upload", produces = "text/html;charset=utf-8")
 	public String Hello(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
 		return "hello";
+	}
+	
+	@RequestMapping(value = "/test", produces = "text/html;charset=utf-8")
+	@ResponseBody
+	public String test(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
+		return "中文乱码";
 	}
 }
