7ccf7ef12a8e7b49642d5a1f71a70d8153c2ed82
yoshow <ruanyu@live.com>
2017-09-02 20:52:06 +0800

    Init

---
+++ src/main/java/com/x3platform/web/controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.x3platform.web.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	/**
+	 * 登陆界面
+	 */
+	@RequestMapping("hello")
+	public String index(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
+		return "index";
+	}
+	
+	/**
+	 * 登陆界面
+	 */
+	@RequestMapping("hello")
+	public String Hello(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
+		return "hello";
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+abc 中文
+</body>
+</html>
