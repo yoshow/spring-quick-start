c7ede56d0ea30a9a8cb742883d0cfaf0aa78716e
yoshow <ruanyu@live.com>
2017-09-03 22:37:41 +0800

    05-spring-upload-file

--- src/main/java/com/x3platform/web/controllers/HomeController.java
+++ src/main/java/com/x3platform/web/controllers/HomeController.java
@@ -8,20 +8,185 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.List;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import org.apache.tools.zip.ZipEntry;
+import org.apache.tools.zip.ZipFile;
+import org.apache.tools.zip.ZipOutputStream;
+
 @Controller
 public class HomeController {
 	/**
 	 * 登陆界面
 	 */
-	// @RequestMapping("upload")
-	@RequestMapping(value = "/upload", produces = "text/html;charset=utf-8")
+	// @RequestMapping("upload")index
+	@RequestMapping("/upload")
 	public String Hello(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
-		return "hello";
+		// 得到上传文件的保存目录，将上传的文件存放于WEB-INF目录下，不允许外界直接访问，保证上传文件的安全
+		String savePath = this.getClass().getResource("/").getPath() + "uploads";
+		File file = new File(savePath);
+		// 判断上传文件的保存目录是否存在
+		if (!file.exists() && !file.isDirectory()) {
+			System.out.println(savePath + "目录不存在，需要创建");
+			// 创建目录
+			file.mkdir();
+		}
+		// 消息提示
+		String message = "";
+		try {
+			// 使用Apache文件上传组件处理文件上传步骤：
+			// 1、创建一个DiskFileItemFactory工厂
+			DiskFileItemFactory factory = new DiskFileItemFactory();
+			// 2、创建一个文件上传解析器
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			// 解决上传文件名的中文乱码
+			upload.setHeaderEncoding("UTF-8");
+			// 3、判断提交上来的数据是否是上传表单的数据
+			if (!ServletFileUpload.isMultipartContent(request)) {
+				// 按照传统方式获取数据
+				return "x";
+			}
+
+			// 4、使用ServletFileUpload解析器解析上传数据，解析结果返回的是一个List<FileItem>集合，每一个FileItem对应一个Form表单的输入项
+			List<FileItem> list = upload.parseRequest(request);
+			for (FileItem item : list) {
+				// 如果fileitem中封装的是普通输入项的数据
+				if (item.isFormField()) {
+					String name = item.getFieldName();
+					// 解决普通输入项的数据的中文乱码问题
+					String value = item.getString("UTF-8");
+					// value = new String(value.getBytes("iso8859-1"),"UTF-8");
+					System.out.println(name + "=" + value);
+				} else {// 如果fileitem中封装的是上传文件
+						// 得到上传的文件名称，
+					String filename = item.getName();
+					System.out.println(filename);
+					if (filename == null || filename.trim().equals("")) {
+						continue;
+					}
+					// 注意：不同的浏览器提交的文件名是不一样的，有些浏览器提交上来的文件名是带有路径的，如：
+					// c:\a\b\1.txt，而有些只是单纯的文件名，如：1.txt
+					// 处理获取到的上传文件的文件名的路径部分，只保留文件名部分
+					filename = filename.substring(filename.lastIndexOf("\\") + 1);
+					// 获取item中的上传文件的输入流
+					InputStream in = item.getInputStream();
+					// 创建一个文件输出流
+					FileOutputStream out = new FileOutputStream(savePath + "\\" + filename);
+					// 创建一个缓冲区
+					byte buffer[] = new byte[1024];
+					// 判断输入流中的数据是否已经读完的标识
+					int len = 0;
+					// 循环将输入流读入到缓冲区当中，(len=in.read(buffer))>0就表示in里面还有数据
+					while ((len = in.read(buffer)) > 0) {
+						// 使用FileOutputStream输出流将缓冲区的数据写入到指定的目录(savePath + "\\"
+						// + filename)当中
+						out.write(buffer, 0, len);
+					}
+					// 关闭输入流
+					in.close();
+					// 关闭输出流
+					out.close();
+					// 删除处理文件上传时生成的临时文件
+					item.delete();
+					message = "文件上传成功！";
+					
+					unzip(savePath + "\\" + filename,savePath + "\\temp" );
+				}
+			}
+		} catch (Exception e) {
+			message = "文件上传失败！";
+			e.printStackTrace();
+
+		}
+
+		// request.setAttribute("message", message);
+		// request.getRequestDispatcher("/message.jsp").forward(request,
+		// response);
+
+		return "index";
 	}
-	
-	@RequestMapping(value = "/test", produces = "text/html;charset=utf-8")
-	@ResponseBody
+
+	private static final int BUFFEREDSIZE = 1024;
+
+	/**
+	 * 解压zip或者rar包的内容到指定的目录下，可以处理其文件夹下包含子文件夹的情况
+	 * 
+	 * @param zipFilename
+	 *            要解压的zip或者rar包文件
+	 * @param outputDirectory
+	 *            解压后存放的目录
+	 */
+	public synchronized void unzip(String zipFilename, String outputDirectory) throws IOException {
+		File outFile = new File(outputDirectory);
+		if (!outFile.exists()) {
+			outFile.mkdirs();
+		}
+		
+		System.setProperty("sun.zip.encoding", System.getProperty("sun.jnu.encoding"));
+		
+		ZipFile zipFile = new ZipFile(zipFilename);
+		Enumeration en = zipFile.getEntries();
+		ZipEntry zipEntry = null;
+		while (en.hasMoreElements()) {
+			zipEntry = (ZipEntry) en.nextElement();
+			if (zipEntry.isDirectory()) {
+				// mkdir directory
+				String dirName = zipEntry.getName();
+				// System.out.println("=dirName is:=" + dirName + "=end=");
+				dirName = dirName.substring(0, dirName.length() - 1);
+				File f = new File(outFile.getPath() + File.separator + dirName);
+				f.mkdirs();
+			} else {
+				// unzip file
+				String strFilePath = outFile.getPath() + File.separator + zipEntry.getName();
+				File f = new File(strFilePath);
+
+				// the codes remedified by can_do on 2010-07-02 =begin=
+				// /////begin/////
+				// 判断文件不存在的话，就创建该文件所在文件夹的目录
+				if (!f.exists()) {
+					String[] arrFolderName = zipEntry.getName().split("/");
+					String strRealFolder = "";
+					for (int i = 0; i < (arrFolderName.length - 1); i++) {
+						strRealFolder += arrFolderName[i] + File.separator;
+					}
+					strRealFolder = outFile.getPath() + File.separator + strRealFolder;
+					File tempDir = new File(strRealFolder);
+					// 此处使用.mkdirs()方法，而不能用.mkdir()
+					tempDir.mkdirs();
+				}
+				// /////end///
+				// the codes remedified by can_do on 2010-07-02 =end=
+				f.createNewFile();
+				InputStream in = zipFile.getInputStream(zipEntry);
+				FileOutputStream out = new FileOutputStream(f);
+				try {
+					int c;
+					byte[] by = new byte[BUFFEREDSIZE];
+					while ((c = in.read(by)) != -1) {
+						out.write(by, 0, c);
+					}
+					// out.flush();
+				} catch (IOException e) {
+					throw e;
+				} finally {
+					out.close();
+					in.close();
+				}
+			}
+		}
+	}
+
+	@RequestMapping("/index")
 	public String test(HttpServletRequest request, HttpServletResponse response, ModelMap map) {
-		return "中文乱码";
+		return "index";
 	}
 }
